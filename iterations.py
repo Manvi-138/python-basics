ca7a8ce8af21393a9a4803608eea12d49790f969
Manvi-138 <128123479+Manvi-138@users.noreply.github.com>
2023-03-17 11:57:27 +0530

    Update iterations.py

--- iterations.py
+++ iterations.py
@@ -1,1 +1,12 @@
-https://colab.research.google.com/drive/1dOEhUGTSpzCJlQxZSwMECLMfbuOjNEUJ#scrollTo=EVxKUSN3JuoZ
+import numpy as np
+from numpy.random import random
+answer=None
+x=random()
+if x>0.8:
+  answer="greater than o.8"
+elif x==0.8:
+  answer="equal to 0.8"
+else:
+    answer="less than 0.8"
+print(x)
+print(answer)
