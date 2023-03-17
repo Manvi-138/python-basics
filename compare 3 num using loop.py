14f7b4057afe021a07f1fade944ae532f5c69a05
Manvi-138 <128123479+Manvi-138@users.noreply.github.com>
2023-03-17 12:16:47 +0530

    Create compare 3 num using loop.py

---
+++ compare 3 num using loop.py
@@ -0,0 +1,9 @@
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+c=int(input("enter third number"))
+if(a>b&a>c):
+  print("a is greatest among all")
+elif(b>a&b>c):
+  print("b is greatest")
+else:
+  print("c is greatest")
