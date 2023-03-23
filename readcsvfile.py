aa6c507ebcfcb73a8d0e1ab6b8ff8ed0d9d998ee
Manvi-138 <128123479+Manvi-138@users.noreply.github.com>
2023-03-23 12:44:53 +0530

    Update readcsvfile.py

--- readcsvfile.py
+++ readcsvfile.py
@@ -1,3 +1,5 @@
+#read csv file through pandas
 import pandas as pd
 data=pd.read_csv("Students.csv")
-data
+data.head(6) #to read first 6 values from csv file
+#data.tail(4)-to read last 4 values from csv file
